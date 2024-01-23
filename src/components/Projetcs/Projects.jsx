d7ddae7c474cd91c2785503c775c47d883fde88b
AltamiroF-22 <altamiroribeirodarocha@gmail.com>
2024-01-22 22:34:01 -0300

    Projects sec

--- src/components/Projetcs/Projects.jsx
+++ src/components/Projetcs/Projects.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-target-blank */
+// -----------------------------------------------------------------------------
+
 //imports
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 //components
 import MagnetoCircle from "../MagnetoCircle/MagnetoCircle";
 // style
 import "./Projects.sass";
 //icons
 import { TbApi } from "react-icons/tb";
-import { FaReact, FaSass } from "react-icons/fa";
+import { FaReact, FaSass, FaHtml5, FaCss3Alt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 //images
 import RestCountry from "../../imgs/projects/rest-country.png";
@@ -18,35 +20,6 @@ import Space from "../../imgs/projects/Space.png";
 import DisplayFlexCfg from "../../imgs/projects/dp-flex.png";
 
 const Projects = () => {
-  // useEffect(() => {
-  //   const divCursor = document.createElement("div");
-  //   divCursor.classList.add("divCursor");
-  //   divCursor.style.position = "fixed";
-  
-  //   document.body.appendChild(divCursor);
-  
-  //   let timeoutId;
-  
-  //   const handleMouseMove = (event) => {
-  //     clearTimeout(timeoutId);
-  
-  //     timeoutId = setTimeout(() => {
-  //       const mouseX = event.pageX;
-  //       const mouseY = event.pageY;
-  //       divCursor.style.left = `${mouseX}px`;
-  //       divCursor.style.top = `${mouseY}px`;
-  //     }, 2); // Ajuste o valor do timeout conforme necessário
-  //   };
-  
-  //   document.addEventListener("mousemove", handleMouseMove);
-  
-  //   return () => {
-  //     document.removeEventListener("mousemove", handleMouseMove);
-  //     document.body.removeChild(divCursor);
-  //   };
-  // }, []);
-  
-
   return (
     <main id="projects" className="projects">
       <MagnetoCircle text={"Projects"} backgroundColor={"orange"} />
@@ -59,13 +32,10 @@ const Projects = () => {
           <div className="content  first">
             <h1>REST Coutrie API</h1>
             <div className="content-main">
-              <h3>Responsive game built with React + SASS</h3>
-              <div className="built-with">
-                <TbApi />
-                <FaReact />
-                <IoLogoJavascript />
-                <FaSass />
-              </div>
+              <TbApi />
+              <FaReact />
+              <IoLogoJavascript />
+              <FaSass />
             </div>
             <div className="project-image">
               <img src={RestCountry} alt="rest contry image" />
@@ -74,19 +44,15 @@ const Projects = () => {
         </a>
         <a
           target="_blank"
-          href="https://snazzy-malasada-b4cc16.netlify.app/"
+          href="https://magical-duckanoo-d9dbfc.netlify.app/"
           className="container"
         >
           <div className="content">
-            <h1>REST Coutrie API</h1>
+            <h1>Rock Paper Scissors</h1>
             <div className="content-main">
-              <h3>Responsive game built with React + SASS</h3>
-              <div className="built-with">
-                <TbApi />
-                <FaReact />
-                <IoLogoJavascript />
-                <FaSass />
-              </div>
+              <FaReact />
+              <IoLogoJavascript />
+              <FaSass />
             </div>
             <div className="project-image">
               <img src={RockPaper} alt="rest contry image" />
@@ -95,19 +61,17 @@ const Projects = () => {
         </a>
         <a
           target="_blank"
-          href="https://snazzy-malasada-b4cc16.netlify.app/"
+          href="https://elegant-fudge-d486ff.netlify.app/"
           className="container"
         >
           <div className="content">
-            <h1>REST Coutrie API</h1>
+            <h1>IP Address Tracker</h1>
+
             <div className="content-main">
-              <h3>Responsive game built with React + SASS</h3>
-              <div className="built-with">
-                <TbApi />
-                <FaReact />
-                <IoLogoJavascript />
-                <FaSass />
-              </div>
+              <TbApi />
+              <FaReact />
+              <IoLogoJavascript />
+              <FaSass />
             </div>
             <div className="project-image">
               <img src={IpAddress} alt="rest contry image" />
@@ -116,19 +80,15 @@ const Projects = () => {
         </a>
         <a
           target="_blank"
-          href="https://snazzy-malasada-b4cc16.netlify.app/"
+          href="https://cerulean-gingersnap-90ed6f.netlify.app/"
           className="container"
         >
           <div className="content">
-            <h1>REST Coutrie API</h1>
+            <h1>Space Tourism </h1>
             <div className="content-main">
-              <h3>Responsive game built with React + SASS</h3>
-              <div className="built-with">
-                <TbApi />
-                <FaReact />
-                <IoLogoJavascript />
-                <FaSass />
-              </div>
+              <FaReact />
+              <IoLogoJavascript />
+              <FaSass />
             </div>
             <div className="project-image">
               <img src={Space} alt="rest contry image" />
@@ -137,25 +97,40 @@ const Projects = () => {
         </a>
         <a
           target="_blank"
-          href="https://snazzy-malasada-b4cc16.netlify.app/"
+          href="https://64efd8178cf9c304f36e4ead--deluxe-profiterole-fea8eb.netlify.app/"
           className="container"
         >
           <div className="content">
-            <h1>REST Coutrie API</h1>
+            <h1>Display Flex Config</h1>
             <div className="content-main">
-              <h3>Responsive game built with React + SASS</h3>
-              <div className="built-with">
-                <TbApi />
-                <FaReact />
-                <IoLogoJavascript />
-                <FaSass />
-              </div>
+              <FaHtml5 />
+              <FaCss3Alt />
+              <IoLogoJavascript />
             </div>
             <div className="project-image">
               <img src={DisplayFlexCfg} alt="rest contry image" />
             </div>
           </div>
         </a>
+        {/* <a
+          target="_blank"
+          href="https://64dafdef853a3a1e79751fc2--iridescent-twilight-603d5e.netlify.app/"
+          className="container"
+        >
+          <div className="content">
+            <h1>Display Flex Config</h1>
+          
+              <div className="built-with">
+                <FaHtml5 />
+                <FaCss3Alt />
+                <IoLogoJavascript />
+            
+            </div>
+            <div className="project-image">
+              <img src={DisplayFlexCfg} alt="rest contry image" />
+            </div>
+          </div>
+        </a> */}
       </section>
     </main>
   );
